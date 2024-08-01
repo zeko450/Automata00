ff07ab398e51e3a7425416251db35e61576aa0c9
Mohamed Kachach <114595632+zeko450@users.noreply.github.com>
2024-08-01 18:18:14 -0400

    Add files via upload

---
+++ src/tdfinal/Tdfinal.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tdfinal;
+
+/**
+ *
+ * @author moh-k
+ */
+public class Tdfinal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bonjour" + args[1] + ", " + args[0]);
+    }
+}
